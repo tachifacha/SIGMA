f367ad804ecfbaf98e33c79fd8c402b86ce2c729
tachifacha <octavio.artico@gmail.com>
2026-05-29 11:03:05 -0300

    quitar codigo repetido

--- src/modulos/compras_provision/proveedores/eliminarProv.php
+++ src/modulos/compras_provision/proveedores/eliminarProv.php
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-<?php
-session_start();
-require "../../../config/conexion.php";
-
-$pdo = db::connect();
-
-$id = $_GET["id"] ?? null;
-
-if ($id) {
-    try {
-        $stmt = $pdo->prepare("DELETE FROM proveedores WHERE id_proveedor=?");
-        $stmt->execute([$id]);
-        $_SESSION["flash_success"][] = "Proveedor eliminado correctamente";
-        header("Location: consultarProv.php");
-        exit();
-    } catch (Exception $e) {
-        $_SESSION["flash_errors"] = [
-            "Error al eliminar proveedor: " . $e->getMessage(),
-        ];
-        header("Location: consultarProv.php");
-        exit();
-    }
-}
-?>
-=======
 <?php
 session_start();
 require "../../../config/conexion.php";
@@ -48,4 +22,3 @@ if ($id) {
     }
 }
 ?>
->>>>>>> 52d952fb4caf85db35038f9d8634db609af918dc
